2aba2dced73e4ae0d32105f00eb37bc0205e34ae
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-06-08 23:25:46 +0000

    commit

--- src/app/admin/shop/page.tsx
+++ src/app/admin/shop/page.tsx
@@ -7,6 +7,15 @@ export default async function Storefront() {
 
   return (
     <>
+      <div className="mb-10">
+        <h2 className="font-semibold text-lg mb-5">Elements</h2>
+        <div className="flex gap-2">
+          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Page hero</div>
+          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Categories</div>
+          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Recommendations</div>
+        </div>
+      </div>
+      <h2 className="font-semibold text-lg mb-5">Collections</h2>
       <CollectionTable collections={collections} />
       <NewCollectionOverlay />
     </>
