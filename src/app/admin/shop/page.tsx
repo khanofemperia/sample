cb3f3c0e54c6c8e1854425902365cabaf88add08
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-06-09 09:00:03 +0000

    commit

--- src/app/admin/shop/page.tsx
+++ src/app/admin/shop/page.tsx
@@ -1,6 +1,7 @@
 import CollectionTable from "@/components/admin/Storefront/CollectionTable";
 import { NewCollectionOverlay } from "@/components/admin/Storefront/NewCollection";
 import { fetchData } from "@/libraries/utils";
+import clsx from "clsx";
 
 export default async function Storefront() {
   const collections = await fetchData<CollectionType[]>("/api/collections");
@@ -10,9 +11,90 @@ export default async function Storefront() {
       <div className="mb-10">
         <h2 className="font-semibold text-lg mb-5">Elements</h2>
         <div className="flex gap-2">
-          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Page hero</div>
-          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Categories</div>
-          <div className="w-60 h-20 border rounded-md p-5 font-semibold text-sm bg-white">Recommendations</div>
+          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between">
+              <h2 className="text-left font-semibold text-sm">Page hero</h2>
+              <div
+                className={clsx(
+                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  {
+                    "bg-white border": false,
+                    "bg-custom-blue border border-custom-blue": true,
+                  }
+                )}
+              >
+                <div
+                  className={clsx(
+                    "w-[10px] h-[10px] rounded-full ease-in-out duration-300 absolute [top:50%] [transform:translateY(-50%)]",
+                    {
+                      "left-[5px] bg-black": false,
+                      "left-[23px] bg-white": true,
+                    }
+                  )}
+                ></div>
+              </div>
+            </div>
+            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+              It's the first thing visitors notice; use visuals that make a
+              strong first impression.
+            </p>
+          </button>
+          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between">
+              <h2 className="text-left font-semibold text-sm">Categories</h2>
+              <div
+                className={clsx(
+                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  {
+                    "bg-white border": false,
+                    "bg-custom-blue border border-custom-blue": true,
+                  }
+                )}
+              >
+                <div
+                  className={clsx(
+                    "w-[10px] h-[10px] rounded-full ease-in-out duration-300 absolute [top:50%] [transform:translateY(-50%)]",
+                    {
+                      "left-[5px] bg-black": false,
+                      "left-[23px] bg-white": true,
+                    }
+                  )}
+                ></div>
+              </div>
+            </div>
+            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+              Group similar products so they're easy to find: Dresses, Tops,
+              Bottoms, Outerwear, Shoes, Accessories.
+            </p>
+          </button>
+          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between">
+              <h2 className="text-left font-semibold text-sm">Page hero</h2>
+              <div
+                className={clsx(
+                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  {
+                    "bg-white border": false,
+                    "bg-custom-blue border border-custom-blue": true,
+                  }
+                )}
+              >
+                <div
+                  className={clsx(
+                    "w-[10px] h-[10px] rounded-full ease-in-out duration-300 absolute [top:50%] [transform:translateY(-50%)]",
+                    {
+                      "left-[5px] bg-black": false,
+                      "left-[23px] bg-white": true,
+                    }
+                  )}
+                ></div>
+              </div>
+            </div>
+            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+              This is a treasure hunt - a catalog featuring products from all
+              categories. Users scroll and the surprises roll in.
+            </p>
+          </button>
         </div>
       </div>
       <h2 className="font-semibold text-lg mb-5">Collections</h2>
