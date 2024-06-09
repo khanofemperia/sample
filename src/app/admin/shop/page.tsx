897d3a6f9f0a5c6b12be04f18d3d144ee0359bed
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-06-09 09:13:04 +0000

    commit

--- src/app/admin/shop/page.tsx
+++ src/app/admin/shop/page.tsx
@@ -11,12 +11,12 @@ export default async function Storefront() {
       <div className="mb-10">
         <h2 className="font-semibold text-lg mb-5">Elements</h2>
         <div className="flex gap-2">
-          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
-            <div className="w-full mb-4 flex items-center justify-between">
+          <button className="flex flex-col items-start w-64 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between relative">
               <h2 className="text-left font-semibold text-sm">Page hero</h2>
               <div
                 className={clsx(
-                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  "relative w-10 h-5 rounded-full ease-in-out duration-200",
                   {
                     "bg-white border": false,
                     "bg-custom-blue border border-custom-blue": true,
@@ -34,17 +34,17 @@ export default async function Storefront() {
                 ></div>
               </div>
             </div>
-            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+            <p className="w-48 text-left text-gray text-xs leading-[18px]">
               It's the first thing visitors notice; use visuals that make a
               strong first impression.
             </p>
           </button>
-          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
-            <div className="w-full mb-4 flex items-center justify-between">
+          <button className="flex flex-col items-start w-64 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between relative">
               <h2 className="text-left font-semibold text-sm">Categories</h2>
               <div
                 className={clsx(
-                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  "relative w-10 h-5 rounded-full ease-in-out duration-200",
                   {
                     "bg-white border": false,
                     "bg-custom-blue border border-custom-blue": true,
@@ -62,17 +62,17 @@ export default async function Storefront() {
                 ></div>
               </div>
             </div>
-            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+            <p className="w-48 text-left text-gray text-xs leading-[18px]">
               Group similar products so they're easy to find: Dresses, Tops,
               Bottoms, Outerwear, Shoes, Accessories.
             </p>
           </button>
-          <button className="w-60 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
-            <div className="w-full mb-4 flex items-center justify-between">
+          <button className="flex flex-col items-start w-64 rounded-md p-5 relative cursor-pointer ease-in-out duration-300 transition shadow border border-transparent bg-white active:border-[#bfc5ce] lg:hover:border-[#bfc5ce]">
+            <div className="w-full mb-4 flex items-center justify-between relative">
               <h2 className="text-left font-semibold text-sm">Shop now</h2>
               <div
                 className={clsx(
-                  "absolute top-5 right-5 w-10 h-5 rounded-full ease-in-out duration-200",
+                  "relative w-10 h-5 rounded-full ease-in-out duration-200",
                   {
                     "bg-white border": false,
                     "bg-custom-blue border border-custom-blue": true,
@@ -90,7 +90,7 @@ export default async function Storefront() {
                 ></div>
               </div>
             </div>
-            <p className="w-44 text-left text-gray text-xs leading-[18px]">
+            <p className="w-48 text-left text-gray text-xs leading-[18px]">
               This is a treasure hunt - a catalog featuring products from all
               categories. Users scroll and the surprises roll in.
             </p>
