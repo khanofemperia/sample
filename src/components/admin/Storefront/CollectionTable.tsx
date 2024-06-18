08ef3e9ed8d3856b6bc9398289c68a9e23c97b21
Bundlestand <141491831+Bundlestand-v1@users.noreply.github.com>
2024-06-18 12:32:31 +0000

    commit

--- src/components/admin/Storefront/CollectionTable.tsx
+++ src/components/admin/Storefront/CollectionTable.tsx
@@ -45,7 +45,7 @@ export default function CollectionTable({
     };
   };
 
-  const rowsPerPage = 8;
+  const rowsPerPage = 5;
   const { paginatedArray: tableData, totalPages } = pagination(
     collections,
     currentPage,
