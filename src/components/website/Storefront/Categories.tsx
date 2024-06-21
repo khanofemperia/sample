58edcd198f57eed15f5c23f2de57dd55a2fee75c
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-06-21 11:41:29 +0000

    commit

--- src/components/website/Storefront/Categories.tsx
+++ src/components/website/Storefront/Categories.tsx
@@ -2,9 +2,22 @@
 
 import { ChevronLeftIcon, ChevronRightIcon } from "@/icons";
 import { capitalizeFirstLetter } from "@/libraries/utils";
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const INITIAL_CATEGORY_WIDTH = 110;
+const LARGE_SCREEN_CATEGORY_WIDTH = 120;
+const GAP_BETWEEN_CATEGORIES = 16;
+const MINIMUM_CATEGORIES_FOR_NAVIGATION = 2;
+const SCREEN_WIDTH_LARGE = 1024;
+const SCREEN_WIDTH_THRESHOLD = 808;
+const CATEGORIES_THRESHOLD = 6;
+const SLICE_SIZE_INITIAL = 3;
+const SLICE_SIZE_MAX = 10;
+const WIDTH_THRESHOLD_INITIAL = 398;
+const WIDTH_THRESHOLD_INCREMENT = 126;
 
 export default function Categories({
   categories,
@@ -13,112 +26,151 @@ export default function Categories({
 }) {
   const [distance, setDistance] = useState(0);
   const [isPrevButtonHidden, setIsPrevButtonHidden] = useState(true);
-  const [isNextButtonHidden, setIsNextButtonHidden] = useState(false);
-  const [maxCategoriesShown, setMaxCategoriesShown] = useState(2);
-  const [categoryWidth, setCategoryWidth] = useState(110);
+  const [isNextButtonHidden, setIsNextButtonHidden] = useState(true);
+  const [categoryWidth, setCategoryWidth] = useState(INITIAL_CATEGORY_WIDTH);
+  const [shouldTransition, setShouldTransition] = useState(true);
 
-  const gapBetweenCategories = 16;
+  const carouselRef = useRef(null);
+
+  const resetCarousel = (screenWidth: number) => {
+    setShouldTransition(false);
+
+    // Reset distance
+    setDistance(0);
+
+    // Reset button visibility
+    setIsPrevButtonHidden(true);
+
+    // Determine if next button should be hidden based on categories and screen width
+    if (categories.length <= MINIMUM_CATEGORIES_FOR_NAVIGATION) {
+      setIsNextButtonHidden(true);
+    } else {
+      let sliceSize = SLICE_SIZE_INITIAL;
+      let widthThreshold = WIDTH_THRESHOLD_INITIAL;
+
+      while (sliceSize <= SLICE_SIZE_MAX) {
+        if (
+          categories.slice(0, sliceSize).length >= sliceSize &&
+          screenWidth < widthThreshold
+        ) {
+          setIsNextButtonHidden(false);
+          break;
+        }
+        sliceSize++;
+        widthThreshold += WIDTH_THRESHOLD_INCREMENT;
+      }
+
+      // If we've gone through all slice sizes and haven't set isNextButtonHidden to false
+      if (sliceSize > SLICE_SIZE_MAX) {
+        setIsNextButtonHidden(true);
+      }
+    }
+
+    // Reset category width based on screen size
+    if (screenWidth >= SCREEN_WIDTH_LARGE) {
+      setCategoryWidth(LARGE_SCREEN_CATEGORY_WIDTH);
+    } else {
+      setCategoryWidth(INITIAL_CATEGORY_WIDTH);
+    }
+  };
 
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
-
-      if (screenWidth >= 1024) {
-        setCategoryWidth(120);
-      }
-
-      if (screenWidth >= 900) {
-        setMaxCategoriesShown(7);
-      } else if (screenWidth >= 774) {
-        setMaxCategoriesShown(6);
-      } else if (screenWidth >= 648) {
-        setMaxCategoriesShown(5);
-      } else if (screenWidth >= 522) {
-        setMaxCategoriesShown(4);
-      } else if (screenWidth >= 396) {
-        setMaxCategoriesShown(3);
-      } else {
-        setMaxCategoriesShown(2);
-      }
+      resetCarousel(screenWidth);
     };
 
     handleResize();
 
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [categories]);
 
   useEffect(() => {
-    setIsPrevButtonHidden(distance === 0);
-
     const maxDistance =
-      (categories.length - maxCategoriesShown) *
-      (categoryWidth + gapBetweenCategories);
-    setIsNextButtonHidden(distance <= -maxDistance);
-  }, [distance, categories.length, maxCategoriesShown]);
+      (categories.length - 1) * (categoryWidth + GAP_BETWEEN_CATEGORIES);
+    const screenWidth = window.innerWidth;
+    let totalRemainingDistance = maxDistance;
+
+    if (
+      screenWidth >= SCREEN_WIDTH_THRESHOLD &&
+      categories.length > CATEGORIES_THRESHOLD
+    ) {
+      totalRemainingDistance =
+        (categories.length - CATEGORIES_THRESHOLD) *
+        (categoryWidth + GAP_BETWEEN_CATEGORIES);
+      setIsNextButtonHidden(distance <= -totalRemainingDistance);
+    } else {
+      const visibleWidth =
+        categories.length * (categoryWidth + GAP_BETWEEN_CATEGORIES);
+      setIsNextButtonHidden(visibleWidth + distance <= screenWidth);
+    }
+
+    setIsPrevButtonHidden(distance === 0);
+  }, [distance, categories.length, categoryWidth]);
 
   const handleNext = () => {
+    setShouldTransition(true);
+
     const maxDistance =
-      (categories.length - maxCategoriesShown) *
-      (categoryWidth + gapBetweenCategories);
+      (categories.length - 1) * (categoryWidth + GAP_BETWEEN_CATEGORIES);
     if (distance > -maxDistance) {
-      setDistance(distance - (categoryWidth + gapBetweenCategories));
+      setDistance(distance - (categoryWidth + GAP_BETWEEN_CATEGORIES));
     }
   };
 
   const handlePrev = () => {
+    setShouldTransition(true);
+
     if (distance < 0) {
-      setDistance(
-        Math.min(0, distance + (categoryWidth + gapBetweenCategories))
-      );
+      setDistance(distance + (categoryWidth + GAP_BETWEEN_CATEGORIES));
     }
   };
 
   return (
-    <div className="relative mb-10 w-full max-w-[888px] lg:max-w-[942px] mx-auto">
-      <div className="overflow-hidden">
+    <div className="relative mb-10 w-full max-w-[808px] lg:max-w-[808px] mx-auto">
+      <div className="overflow-hidden bg-red-300" ref={carouselRef}>
         <div
           style={{ transform: `translateX(${distance}px)` }}
-          className="w-max flex gap-4 p-1 transition duration-500 ease-in-out"
+          className={clsx("w-max flex gap-4 p-1 bg-lime-300", {
+            "ease-in-out duration-300 transition": shouldTransition,
+          })}
         >
-          {categories
-            // .filter((category) => category.visibility === "PUBLISHED")
-            .map(({ index, name, image }) => (
-              <Link
-                key={index}
-                href="#"
-                className="first:ml-4 lg:first:ml-0 flex flex-col gap-2 items-center rounded-xl p-[10px] ease-in-out duration-300 transition hover:shadow-[0px_0px_4px_rgba(0,0,0,0.35)]"
-              >
-                <div className="lg:hidden w-[90px] h-[90px] rounded-full shadow-[rgba(0,0,0,0.2)_0px_1px_3px_0px,_rgba(27,31,35,0.15)_0px_0px_0px_1px]">
-                  <div className="w-full h-full rounded-full overflow-hidden flex items-center justify-center z-10">
-                    <Image
-                      src={`/images/categories/${image}`}
-                      alt={name}
-                      width={90}
-                      height={90}
-                      priority={true}
-                    />
-                  </div>
+          {categories.map(({ index, name, image }) => (
+            <Link
+              key={index}
+              href="#"
+              className="first:ml-4 lg:first:ml-0 flex flex-col gap-2 items-center rounded-xl p-[10px] ease-in-out duration-300 transition hover: shadow-[0px_0px_4px_rgba(0,0,0,0.35)]"
+            >
+              <div className="lg:hidden w-[90px] h-[90px] rounded-full shadow-[rgba(0,0,0,0.2)_0px_1px_3px_0px,_rgba(27,31,35,0.15)_0px_0px_0px_1px]">
+                <div className="w-full h-full rounded-full overflow-hidden flex items-center justify-center z-10">
+                  <Image
+                    src={`/images/categories/${image}`}
+                    alt={name}
+                    width={90}
+                    height={90}
+                    priority={true}
+                  />
                 </div>
-                <div className="hidden lg:block w-[100px] h-[100px] rounded-full shadow-[rgba(0,0,0,0.2)_0px_1px_3px_0px,_rgba(27,31,35,0.15)_0px_0px_0px_1px]">
-                  <div className="w-full h-full rounded-full overflow-hidden flex items-center justify-center z-10">
-                    <Image
-                      src={`/images/categories/${image}`}
-                      alt={name}
-                      width={100}
-                      height={100}
-                      priority={true}
-                    />
-                  </div>
+              </div>
+              <div className="hidden lg:block w-[100px] h-[100px] rounded-full shadow-[rgba(0,0,0,0.2)_0px_1px_3px_0px,_rgba(27,31,35,0.15)_0px_0px_0px_1px]">
+                <div className="w-full h-full rounded-full overflow-hidden flex items-center justify-center z-10">
+                  <Image
+                    src={`/images/categories/${image}`}
+                    alt={name}
+                    width={100}
+                    height={100}
+                    priority={true}
+                  />
                 </div>
-                <div className="text-xs font-medium">
-                  {capitalizeFirstLetter(name)}
-                </div>
-              </Link>
-            ))}
+              </div>
+              <div className="text-xs font-medium">
+                {capitalizeFirstLetter(name)}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
       {!isPrevButtonHidden && (
