06ff52897fcfe55f315228ef52c6cbcef0a78f36
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-07-05 08:30:52 +0000

    commit

--- src/app/(website)/shop/[slug]/page.tsx
+++ src/app/(website)/shop/[slug]/page.tsx
@@ -210,7 +210,7 @@ export default function ProductDetails({
                 </div>
                 <div className="flex flex-col gap-5">
                   <span className="font-bold">$49.99</span>
-                  <button className="h-8 w-max px-4 rounded-full flex items-center justify-center gap-[2px] bg-lightgray">
+                  <button className="h-8 w-max px-4 rounded-full flex items-center justify-center gap-[2px] ease-in-out duration-300 transition bg-lightgray hover:bg-lightgray-dimmed">
                     <span className="text-sm font-medium">
                       Select Color & Size
                     </span>
@@ -219,7 +219,7 @@ export default function ProductDetails({
                 </div>
               </div>
               <div
-                className={`${styles.custom_border} mt-7 py-4 px-6 w-max rounded-md bg-white`}
+                className={`${styles.custom_border} mt-7 py-[22px] px-6 w-max rounded-md bg-white`}
               >
                 <div className="w-full">
                   <div>
@@ -358,18 +358,51 @@ export default function ProductDetails({
             </div>
             <div className="h-full flex gap-5 items-center">
               <span className="font-bold">$49.99</span>
-              <span className="text-sm font-medium px-[14px] py-[2px] rounded-full text-[#008a00] bg-custom-green/10">
-                Upgrade $137.99 <strong>(Save 58%)</strong>
-              </span>
+              <button className="h-8 w-max px-4 rounded-full flex items-center justify-center gap-[2px] ease-in-out duration-300 transition bg-lightgray hover:bg-lightgray-dimmed">
+                <span className="text-sm font-medium">Select Color & Size</span>
+                <ChevronRightIcon className="-mr-[7px]" size={20} />
+              </button>
             </div>
           </div>
           <div className="w-[410px] flex gap-3">
             <button className="font-semibold w-full h-12 rounded-full ease-in-out duration-150 transition border border-[rgb(150,150,150)] hover:border-[rgb(80,80,80)] active:border-[rgb(150,150,150)] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.16)]">
               Add to Cart
             </button>
-            <button className="inline-block text-center align-middle h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-custom-amber hover:bg-custom-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
-              Yes, Let's Upgrade
-            </button>
+            <div className="w-full h-12 relative">
+              <button className="inline-block text-center align-middle h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-custom-amber hover:bg-custom-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+                Yes, Let's Upgrade
+              </button>
+              <div className="absolute top-[58px] -right-3 py-[22px] px-6 rounded-xl shadow-dropdown bg-white || before:content-[''] before:w-[14px] before:h-[14px] before:bg-white before:rounded-tl-[2px] before:rotate-45 before:origin-top-left before:absolute before:-top-[10px] before:border-l before:border-t before:border-[#d9d9d9] before:right-24">
+                <div className={`w-max rounded-md bg-white`}>
+                  <div className="w-full">
+                    <div>
+                      <h2 className="font-black text-center text-[21px] text-custom-red leading-6 [letter-spacing:-1px] [word-spacing:2px] [text-shadow:_1px_1px_1px_rgba(0,0,0,0.15)] w-[248px] mx-auto">
+                        UPGRADE MY ORDER
+                      </h2>
+                      <div className="mt-1 text-center font-medium text-gray">
+                        <span className="font-bold text-[#008a00]">
+                          $137.99
+                        </span>{" "}
+                        (Save 58% - was{" "}
+                        <span className="line-through font-medium text-gray">
+                          $189.99
+                        </span>
+                        )
+                      </div>
+                    </div>
+                    <div className="mt-2 h-[210px] aspect-square mx-auto overflow-hidden">
+                      <Image
+                        src="https://i.pinimg.com/564x/ab/d7/1b/abd71b557fc77916f1570da50c0325a8.jpg"
+                        alt="Upgrade my order"
+                        width={240}
+                        height={240}
+                        priority
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
