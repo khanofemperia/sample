bfe617fc1fd7bc2e9ce15b370f3d0db778deeb31
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-07-07 00:48:23 +0000

    commit

--- src/app/(website)/shop/[slug]/page.tsx
+++ src/app/(website)/shop/[slug]/page.tsx
@@ -397,20 +397,20 @@ export default function ProductDetails({
     //                 for every activity!
     //               </p>
     //               <ul className="text-sm list-inside *:leading-[25px]">
-    //                 <li className="flex items-center gap-2">
-    //                   <CheckmarkIcon className="fill-custom-green" size={19} />
+    //                 <li className="flex items-start gap-2">
+    //                   <CheckmarkIcon className="fill-custom-green mt-[3px] -ml-[1px]" size={19} />
     //                   <span>Quick-dry fabric for cool comfort.</span>
     //                 </li>
-    //                 <li className="flex items-center gap-2">
-    //                   <CheckmarkIcon className="fill-custom-green" size={19} />
+    //                 <li className="flex items-start gap-2">
+    //                   <CheckmarkIcon className="fill-custom-green mt-[3px] -ml-[1px]" size={19} />
     //                   <span>Double layer design for better movement.</span>
     //                 </li>
-    //                 <li className="flex items-center gap-2">
-    //                   <CheckmarkIcon className="fill-custom-green" size={19} />
+    //                 <li className="flex items-start gap-2">
+    //                   <CheckmarkIcon className="fill-custom-green mt-[3px] -ml-[1px]" size={19} />
     //                   <span>Zipper pocket to secure your phone.</span>
     //                 </li>
-    //                 <li className="flex items-center gap-2">
-    //                   <CheckmarkIcon className="fill-custom-green" size={19} />
+    //                 <li className="flex items-start gap-2">
+    //                   <CheckmarkIcon className="fill-custom-green mt-[3px] -ml-[1px]" size={19} />
     //                   <span>Ideal for running, gym, and casual wear.</span>
     //                 </li>
     //               </ul>
@@ -586,8 +586,8 @@ export default function ProductDetails({
     // </>
     <>
       <main>
-        <div className="w-full h-[1200px]">
-          <div>
+        <div className="w-full min-h-screen max-h-screen overflow-hidden flex flex-col">
+          <div className="overflow-x-hidden h-[calc(100%-72px)]">
             <div className="w-full max-w-full relative">
               <button
                 type="button"
@@ -597,11 +597,9 @@ export default function ProductDetails({
               </button>
               <ImageCarousel images={images} name={name} />
             </div>
-            <div className="py-6 px-5">
-              <p className="mt-[-6px] text-sm text-gray">
-                High Waisted Running Shorts
-              </p>
-              <div className="mt-4 flex flex-col gap-5">
+            <div className="pb-12 pt-5 px-5">
+              <div className="flex flex-col gap-5">
+                <p className="text-sm text-gray">High Waisted Running Shorts</p>
                 <div className="flex flex-col gap-3">
                   <p className="text-lg leading-[26px]">
                     <strong className="font-bold text-lg leading-[26px]">
@@ -616,27 +614,39 @@ export default function ProductDetails({
                     for every activity!
                   </p>
                   <ul className="text-sm list-inside *:leading-[25px]">
-                    <li className="flex items-center gap-2">
-                      <CheckmarkIcon className="fill-custom-green" size={19} />
+                    <li className="flex items-start gap-2">
+                      <CheckmarkIcon
+                        className="fill-custom-green mt-[3px] -ml-[1px]"
+                        size={19}
+                      />
                       <span>Quick-dry fabric for cool comfort.</span>
                     </li>
-                    <li className="flex items-center gap-2">
-                      <CheckmarkIcon className="fill-custom-green" size={19} />
+                    <li className="flex items-start gap-2">
+                      <CheckmarkIcon
+                        className="fill-custom-green mt-[3px] -ml-[1px]"
+                        size={19}
+                      />
                       <span>Double layer design for better movement.</span>
                     </li>
-                    <li className="flex items-center gap-2">
-                      <CheckmarkIcon className="fill-custom-green" size={19} />
+                    <li className="flex items-start gap-2">
+                      <CheckmarkIcon
+                        className="fill-custom-green mt-[3px] -ml-[1px]"
+                        size={19}
+                      />
                       <span>Zipper pocket to secure your phone.</span>
                     </li>
-                    <li className="flex items-center gap-2">
-                      <CheckmarkIcon className="fill-custom-green" size={19} />
+                    <li className="flex items-start gap-2">
+                      <CheckmarkIcon
+                        className="fill-custom-green mt-[3px] -ml-[1px]"
+                        size={19}
+                      />
                       <span>Ideal for running, gym, and casual wear.</span>
                     </li>
                   </ul>
                 </div>
                 <div className="flex flex-col gap-5">
                   <span className="font-bold">$49.99</span>
-                  <button className="h-8 w-max px-4 rounded-full flex items-center justify-center gap-[2px] ease-in-out duration-300 transition bg-lightgray hover:bg-lightgray-dimmed">
+                  <button className="h-8 w-max px-4 rounded-full flex items-center justify-center gap-[2px] ease-in-out duration-300 transition bg-lightgray active:bg-lightgray-dimmed lg:hover:bg-lightgray-dimmed">
                     <span className="text-sm font-medium">
                       Select Color & Size
                     </span>
@@ -644,9 +654,152 @@ export default function ProductDetails({
                   </button>
                 </div>
               </div>
+              <div
+                className={`${styles.custom_border} mt-7 pt-5 pb-[26px] px-6 w-max rounded-md select-none bg-white`}
+              >
+                <div className="w-full">
+                  <div>
+                    <h2 className="font-black text-center text-[21px] text-custom-red leading-6 [letter-spacing:-1px] [word-spacing:2px] [text-shadow:_1px_1px_1px_rgba(0,0,0,0.15)] w-[248px] mx-auto">
+                      UPGRADE MY ORDER
+                    </h2>
+                    <div className="mt-1 text-center font-medium text-custom-amber-dimmed">
+                      $137.99 (42% Off)
+                    </div>
+                  </div>
+                  <div className="mt-3 h-[210px] aspect-square mx-auto overflow-hidden">
+                    <Image
+                      src="https:i.pinimg.com/564x/ab/d7/1b/abd71b557fc77916f1570da50c0325a8.jpg"
+                      alt="Upgrade my order"
+                      width={240}
+                      height={240}
+                      priority
+                    />
+                  </div>
+                  <div className="w-[200px] mx-auto mt-5 text-xs leading-6 [word-spacing:1px]">
+                    <ul className="*:flex *:justify-between">
+                      <li>
+                        <p className="text-gray">Shorts</p>
+                        <p>
+                          <span className="font-semibold">$67.99</span>{" "}
+                          <span className="line-through text-gray">$79.99</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p className="text-gray">Backpack</p>
+                        <p>
+                          <span className="font-semibold">$41.99</span>{" "}
+                          <span className="line-through text-gray">$99.99</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p className="text-gray">Sneakers</p>
+                        <p>
+                          <span className="font-semibold">$29.99</span>{" "}
+                          <span className="line-through text-gray">$69.99</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p className="text-gray">Hoodie</p>
+                        <p>
+                          <span className="font-semibold">$79.99</span>{" "}
+                          <span className="line-through text-gray">
+                            $189.99
+                          </span>
+                        </p>
+                      </li>
+                      <li className="mt-2 h-7 flex items-center rounded bg-lightgray font-semibold">
+                        <p className="mx-auto">You Save $100.00</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full mt-12 flex flex-col gap-12">
+                <div>
+                  <h2 className="text-[21px] leading-8 mb-4 font-bold">
+                    The Next-Gen Blender
+                  </h2>
+                  <p className="leading-7">
+                    BlendJet 2 serves up big blender power on the go. We created
+                    the BlendJet 2 portable blender so you can make{" "}
+                    <strong>anything you want, anywhere in the world</strong> —
+                    from a mountaintop to your kitchen countertop. It's easy and
+                    convenient to use at home, at work, outdoors, at the gym, in
+                    the car, at the beach, on vacation or wherever the day takes
+                    you.
+                  </p>
+                  <div>
+                    <br />
+                  </div>
+                  <div className="w-full aspect-square rounded-xl overflow-hidden flex items-center justify-center">
+                    <Image
+                      src="https://i.pinimg.com/564x/8e/fe/b1/8efeb1b9afef852636be660f109fa802.jpg"
+                      alt="Fruits"
+                      width={580}
+                      height={580}
+                      priority={true}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <h2 className="text-[21px] leading-8 mb-4 font-bold">
+                    Patented TurboJet Technology
+                  </h2>
+                  <p className="leading-7">
+                    Traditional blenders only use their blades to blend, but we
+                    invented a new method that makes every other blender
+                    obsolete. Our secret weapon? BlendJet 2's stainless steel
+                    blades are offset from the center of the base, which creates
+                    a tornado effect that blasts ingredients into the back of
+                    the jar 275 times per second, resulting in{" "}
+                    <strong>dramatically better blending.</strong>
+                    This technology — combined with a more powerful motor and
+                    doubled battery capacity — makes BlendJet 2{" "}
+                    <strong>five times more powerful than BlendJet One.</strong>
+                  </p>
+                  <div>
+                    <br />
+                  </div>
+                  <div className="w-full aspect-square rounded-xl overflow-hidden flex items-center justify-center">
+                    <Image
+                      src="https://i.pinimg.com/564x/53/be/0c/53be0c721aa59013e6251d64f54ea01d.jpg"
+                      alt="Fruits"
+                      width={580}
+                      height={580}
+                      priority={true}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <h2 className="text-[21px] leading-8 mb-4 font-bold">
+                    Perfect for Everything
+                  </h2>
+                  <p className="leading-7">
+                    BlendJet 2 makes smoothie-bar-quality beverages,
+                    silky-smooth protein shakes, top-shelf mixed drinks and
+                    creamy frozen lattes, plus milkshakes, slushies, baby food,
+                    dips, dressings, sauces, <strong>and so much more.</strong>{" "}
+                    We'll send a new recipe video straight to your inbox each
+                    week to inspire creativity and ensure you get the most out
+                    of your BlendJet 2.
+                  </p>
+                  <div>
+                    <br />
+                  </div>
+                  <div className="w-full aspect-square rounded-xl overflow-hidden flex items-center justify-center">
+                    <Image
+                      src="https://i.pinimg.com/564x/33/d3/9b/33d39bb6a10b39ebe4b96b6aa56d5b84.jpg"
+                      alt="Fruits"
+                      width={580}
+                      height={580}
+                      priority={true}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="fixed left-0 right-0 bottom-0 z-10 pt-2 pb-5 px-[6px] min-[350px]:px-2 border-r bg-white">
+          <div className="h-[72px] pt-2 pb-5 px-[6px] min-[350px]:px-2 bg-white">
             <div className="flex gap-[6px] min-[350px]:gap-2">
               <button className="leading-5 text-[13px] min-[340px]:text-sm font-semibold w-full h-[44px] rounded-full ease-in-out duration-150 transition border border-[rgb(150,150,150)] hover:border-[rgb(80,80,80)] active:border-[rgb(150,150,150)] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.16)]">
                 Add to Cart
